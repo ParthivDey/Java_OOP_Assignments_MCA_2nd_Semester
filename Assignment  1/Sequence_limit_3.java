4299962d4217ee5f9a70c8b34a6a77bbcb5aac04
Parthiv Dey <92042056+ParthivDey@users.noreply.github.com>
2025-03-09 11:03:34 +0530

    Update Sequence_limit_3.java

--- Assignment  1/Sequence_limit_3.java
+++ Assignment  1/Sequence_limit_3.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 class Sequence {
-    // Function to calculate factorial of a number
+    
     int factorial(int n) {
         int fact = 1;
         for (int i = 1; i <= n; i++) {
@@ -10,16 +10,16 @@ class Sequence {
         return fact;
     }
 
-    // Function to print the sequence
+    
     void Display_Sequence(int limit) {
-        int even = 2; // Starting even number
+        int even = 2; 
         for (int i = 0; i < limit; i++) {
             if (i % 2 == 0) {
-                // Even index - Print even number
+                
                 System.out.print(even + ", ");
                 even += 2;
             } else {
-                // Odd index - Print factorial of the index/2+1
+                
                 int fact = factorial(i / 2 + 1);
                 System.out.print(fact + ", ");
             }
