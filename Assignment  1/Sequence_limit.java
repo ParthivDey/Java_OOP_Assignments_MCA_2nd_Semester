febfd5d7fb76d35b335d5dec33857430303ac3f4
Parthiv Dey <92042056+ParthivDey@users.noreply.github.com>
2025-03-09 10:41:25 +0530

    Update Sequence_limit.java

--- Assignment  1/Sequence_limit.java
+++ Assignment  1/Sequence_limit.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 class Sequence {
-    void printSequence(int limit) {
-        int a = 0, b = 1;  // Fibonacci initial values
-        int power = 2;    // Power of 2 initial value
+    void Display_Sequence(int l) {
+        int a = 0, b = 1;  
+        int power = 2;    
         
         System.out.println("The sequence up to " + limit + " terms:");
-        for (int i = 0; i < limit; i++) {
+        for (int i = 0; i < l; i++) {
             if (i % 2 == 0) {
-                // Even index - Fibonacci number
+                
                 System.out.print(a + ", ");
                 int temp = a + b;
                 a = b;
                 b = temp;
             } else {
-                // Odd index - Power of 2
+                
                 System.out.print(power + ", ");
                 power *= 2;
             }
@@ -28,7 +28,7 @@ class Sequence_limit {
         System.out.print("Enter the limit of the sequence: ");
         int limit = sc.nextInt();
         Sequence obj = new Sequence();
-        obj.printSequence(limit);
+        obj.Display_Sequence(limit);
         sc.close();
     }
 }
